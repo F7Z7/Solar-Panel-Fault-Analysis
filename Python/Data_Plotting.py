0d87168b844d1d5b28d20d222c83e0eb69dcea65
farza <farzanrichu610@gmail.com>
2025-07-29 01:33:27 +0530

    set up libraries and imported the data sets

---
+++ Python/Data_Plotting.py
@@ -0,0 +1,5 @@
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+df=pd.read_csv("C:\\Users\\farza\\MATLAB-SIMULINK\\Solar-Panel-Fault-Analysis\\Data_sets\\solar_sim_results_cleaned.csv")
